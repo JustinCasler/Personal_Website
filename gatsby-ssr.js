b0398ee79d945dd31b4bd643366f9471017d2228
JustinCasler <justinlcasler@gmail.com>
2024-06-12 12:25:36 -0400

    pixel ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,17 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
 
- // You can delete this file if you're not using it
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="nextsale-pixel-script"
+      dangerouslySetInnerHTML={{
+        __html: `(function (w,r){w['NextsaleObject']=r;w[r]=w[r]||function(){(w[r].q = w[r].q || []).push(arguments)};})(window, 'nsio');`,
+      }}
+    />,
+    <script
+      key="nextsale-pixel-src"
+      src="https://sdk.nextsale.io/nextsale.min.js?key=pk_10001cd28e7d7607572bf9d865bcb6b6d482c943"
+      async
+    />,
+  ]);
+};
