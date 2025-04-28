777b1d2f52198aa20c9c66057846d5adf8ad1fd0
JustinCasler <justinlcasler@gmail.com>
2025-04-28 15:29:11 -0400

    remove the pixel code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,22 +1,5 @@
-import React from 'react';
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key="custom-config"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(w, r) {
-            w[r] = w[r] || function() {
-              (w[r].q = w[r].q || []).push(arguments);
-            };
-          })(window, 'customConfig');
-
-          customConfig({
-            siteId: '66eb8a8120c20baed38d8182'
-          });
-        `,
-      }}
-    />,
-    <script key="main-script" src="https://app.sellmore.live/scripts/script.js" async />,
-  ]);
-};
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
